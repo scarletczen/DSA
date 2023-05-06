de88080e967f586196ca6552f4680da9401751fb
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-05-06 07:39:13 +0530

    Time: 207 ms (66.67%) | Memory: 59.6 MB (33.33%) - LeetSync

---
+++ number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.ts
@@ -0,0 +1,27 @@
+// Two Pointer Solution 
+function numSubseq(nums: number[], target: number): number {
+ const MOD = 1000000007;
+
+    nums = nums.sort((a, b) => a - b);
+
+    const pows = [1];
+    
+    for (let i = 1; i < nums.length; i++) {
+        pows.push(pows[i - 1] * 2 % MOD);
+    }
+    
+    let left = 0;
+    let right = nums.length - 1;
+    let ans = 0;
+
+    while (left <= right) {
+        if (nums[left] + nums[right] > target) {
+            right--;
+        } else {
+            ans = (ans + pows[right - left]);
+            left++;
+        }
+    }
+
+    return ans % MOD;
+};
