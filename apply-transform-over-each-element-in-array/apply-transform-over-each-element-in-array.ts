060b3d819b3ca7d17469227dfeb7fcb90e6fa47b
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-05-08 07:31:31 +0530

    Time: 45 ms (99.57%) | Memory: 42.8 MB (76.03%) - LeetSync

---
+++ apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.ts
@@ -0,0 +1,8 @@
+// For Loop Simple O(N)
+function map(arr: number[], fn: (n: number, i: number) => number): number[] {
+    let result = [];
+    for (let i=0; i<arr.length; i++){
+        result.push(fn(arr[i],i));
+    }
+    return result;
+};
