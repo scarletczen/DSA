0e2fc0b16e259f9058758098d4358c3ec5095199
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-05-12 23:24:55 +0530

    Time: 69 ms (100.00%) | Memory: 44.9 MB (100.00%) - LeetSync

---
+++ chunk-array/chunk-array.ts
@@ -0,0 +1,12 @@
+// Simple Array Slice Solution
+function chunk(arr: any[], size: number): any[][] {
+    let result = [];
+    let index = 0;
+
+    while (index < arr.length) {
+        result.push(arr.slice(index, index + size));
+        index += size;
+    }
+
+    return result;
+};
