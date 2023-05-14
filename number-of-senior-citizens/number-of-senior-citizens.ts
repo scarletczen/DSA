6c635dbf655ebf9785a994f33f5c9f45e44ac75a
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-05-14 09:19:21 +0530

    Time: 63 ms (100.00%) | Memory: 44.9 MB (100.00%) - LeetSync

--- number-of-senior-citizens/number-of-senior-citizens.ts
+++ number-of-senior-citizens/number-of-senior-citizens.ts
@@ -1,11 +1,9 @@
 // Simple String, Indexes, to Number Solution 
 function countSeniors(details: string[]): number {
     let result:number = 0;
-    for(let i = 0; i < details.length; i++){
-        let currentString = details[i];
-        let age = +(currentString[11] + currentString[12]);
-        if(age > 60){
-            result++;
+    for (let character of details){
+        if(+character.substring(11,13) > 60){
+            result+=1;
         }
     }
     return result;
