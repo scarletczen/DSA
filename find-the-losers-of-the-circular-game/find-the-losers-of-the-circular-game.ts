f4e0df601951f8676bf414eb0469e8b4e576929f
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-05-14 10:48:50 +0530

    Time: 92 ms (100.00%) | Memory: 46.6 MB (100.00%) - LeetSync

--- find-the-losers-of-the-circular-game/find-the-losers-of-the-circular-game.ts
+++ find-the-losers-of-the-circular-game/find-the-losers-of-the-circular-game.ts
@@ -1,28 +1,17 @@
-// SET and Circular Array Solution
+// Simple Circular Array Solution
 function circularGameLosers(n: number, k: number): number[] {
-  const receivedBall = new Set<number>();
-  const players = Array.from({length: n}, (_, i) => i + 1); // Generated Array 1 to N 
-  const notReceivedBall = new Set<number>(players); // Initially, will contain every player
-
-  let ballHolder = players[0]; // First Player
-  let currentPlayerIndex = 0;  // Index of First Player
-  let timesBallWasPassed = 1;
+  const result = Array.from({length:n},(_,index)=>index+1);
+  let currentPlayerIndex = 0;
+  let multiplier = 1;
 
   while (true) {
-     // Base Case
-     if (receivedBall.has(ballHolder)) {
-      return Array.from(notReceivedBall);
+    
+    if (result[currentPlayerIndex] === 0) {
+      return result.filter((item)=>item > 0);
     } else {
-        receivedBall.add(ballHolder);
-        notReceivedBall.delete(ballHolder);
-
-        // Circular Array Index
-        const nextPlayerIndex = (currentPlayerIndex + (timesBallWasPassed*k))%n; 
-        const nextPlayer = players[nextPlayerIndex];
-        ballHolder = nextPlayer;
-
-        currentPlayerIndex = nextPlayerIndex;
-        timesBallWasPassed++;
+      result[currentPlayerIndex] = 0;
+      currentPlayerIndex = (currentPlayerIndex + (multiplier * k )) % n;
+      multiplier += 1;
     }
   }
 };
