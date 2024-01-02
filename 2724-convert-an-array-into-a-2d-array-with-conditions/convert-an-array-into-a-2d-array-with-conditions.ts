7f0d3901000a0f55506f0c748115b6ea323658f0
Abhinav Thapa <abhinavthapa1998@gmail.com>
2024-01-02 08:25:59 +0530

    Time: 176 ms (6.38%) | Memory: 46.9 MB (93.62%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.ts
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.ts
@@ -1,10 +1,15 @@
+// Time O(n), Space O(n)
 function findMatrix(nums: number[]): number[][] {
-    let m = new Map();
+    let occurrence = new Map();
     let matrix = new Array();
-    for(const x of nums) {
-        m.set(x, m.has(x) ? m.get(x)+1 : 0);
-        if(matrix.length <= m.get(x)) matrix.push(new Array());
-        matrix[m.get(x)].push(x);
+    
+    for(const item of nums) {
+        // Store number of occurrences of any element
+        occurrence.set(item, occurrence.has(item) ? occurrence.get(item)+1 : 0);
+        // Create a new sub array for every occurrence of the max occurring element
+        if(matrix.length <= occurrence.get(item)) matrix.push(new Array());
+        matrix[occurrence.get(item)].push(item);
     }
+    
     return matrix;
 };
