afcce7c45aed224792d8d248a69967d4202fc676
Abhinav Thapa <abhinavthapa1998@gmail.com>
2024-01-09 11:12:57 +0530

    Time: 61 ms (85.79%) | Memory: 45.8 MB (21.99%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,10 @@
+function removeDuplicates(nums: number[]): number {
+    let j = 1;
+    for(let i = 1; i<nums.length; i++){
+        if (nums[i] != nums[i-1]){
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    return j;
+};
