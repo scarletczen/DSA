29d9fead94e17064f923137d4eaf84991744303d
Abhinav Thapa <abhinavthapa1998@gmail.com>
2024-01-14 08:32:36 +0530

    Time: 78 ms (88.57%) | Memory: 50.2 MB (42.86%) - LeetSync

---
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.ts
@@ -0,0 +1,15 @@
+function minSteps(s: string, t: string): number {
+    let sum = 0;
+    let count = new Array(26).fill(0);
+
+    for(let i=0; i<s.length; i++){
+        count[s[i].charCodeAt(0) - 'a'.charCodeAt(0)]++;
+        count[t[i].charCodeAt(0) - 'a'.charCodeAt(0)]--;
+    }
+    for(let n of count){
+        if(n>0){
+            sum+=n;
+        }
+    }
+    return sum
+};
