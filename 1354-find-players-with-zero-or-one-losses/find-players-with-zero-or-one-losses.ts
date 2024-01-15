d523e298d084d2c95f3636e096a79e6051257878
Abhinav Thapa <abhinavthapa1998@gmail.com>
2024-01-15 10:48:17 +0530

    Time: 322 ms (71.68%) | Memory: 107.6 MB (84.96%) - LeetSync

---
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.ts
@@ -0,0 +1,13 @@
+function findWinners(matches: number[][]): number[][] {
+  let lost = {};
+  for (let [winner, loser] of matches){
+      lost[winner] = lost[winner] || 0;
+      lost[loser] = (lost[loser] || 0) + 1;
+  }
+  let res = [[],[]];
+  for (let player in lost){
+      if(lost[player] === 0) res[0].push(+player);
+      else if(lost[player] === 1) res[1].push(+player);
+  }  
+  return res;
+};
