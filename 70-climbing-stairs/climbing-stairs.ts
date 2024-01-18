d7d6c0edf3121a5b34b47cdb1de1ef69e762e25e
Abhinav Thapa <abhinavthapa1998@gmail.com>
2024-01-18 08:29:48 +0530

    Time: 63 ms (21.67%) | Memory: 42.3 MB (92.02%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.ts
+++ 70-climbing-stairs/climbing-stairs.ts
@@ -1,19 +1,11 @@
-// DP with Tabulation Time O(N) Space O(1)
-function climbStairs(n: number): number {
-    if (n < 2){
-        return 1;
+function climbStairs(n: number, cache = []): number {
+    if(n in cache){
+        return cache[n]
     }
-    // Base case
-    let firstStep = 1
-    let secondStep  = 1
-    // initialise
-    let thirdStep;
-    for (let i=2; i<n+1; i++){
-        // Set current value
-        thirdStep = firstStep + secondStep;
-        // Move the two previous values forward
-        firstStep = secondStep;
-        secondStep = thirdStep;
+    if(n <= 2){
+        return n
+    } else {
+        cache[n] = climbStairs(n-2, cache) + climbStairs(n-1, cache)
     }
-    return thirdStep;
-}
+    return cache[n]
+};
