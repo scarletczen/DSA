b2fde30d88c4c1a87b6f6128161933752b328655
Abhinav Thapa <abhinavthapa1998@gmail.com>
2024-01-22 20:00:43 +0530

    Time: 71 ms (48.28%) | Memory: 56.2 MB (6.90%) - LeetSync

--- 645-set-mismatch/set-mismatch.ts
+++ 645-set-mismatch/set-mismatch.ts
@@ -1,15 +1,16 @@
+// Time O(N) Space O(1) using arithmetic sum
 function findErrorNums(nums: number[]): number[] {
   let arrayLength = nums.length;
   let sum = (arrayLength*(arrayLength+1))/2;
-  let s=0, act=0;
+  let s=0, repeated=0;
   let obj = {};
   for(let i of nums){
       if(obj[i]){
-          act = i;
+          repeated = i;
       }else{
           obj[i] = true;
           s+=i;
       }
   }
-  return [act, sum-s];
+  return [repeated, sum-s];
 };
