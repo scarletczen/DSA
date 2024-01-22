7e9a4cc2229a0de1b3713c1ea10f452a8605ce1a
Abhinav Thapa <abhinavthapa1998@gmail.com>
2024-01-22 20:07:50 +0530

    Time: 56 ms (53.89%) | Memory: 50 MB (5.10%) - LeetSync

---
+++ 198-house-robber/house-robber.ts
@@ -0,0 +1,16 @@
+// Time O(n) Space O(1) DP Iterative
+function rob(nums: number[]): number {
+    //Base Cases
+    if(!nums.length) return 0;
+    if(nums.length === 1) return nums[0];
+    if(nums.length === 2) return Math.max(nums[0], nums[1]);
+
+    let maxAtTwoBefore = nums[0];
+    let maxAtOneBefore = Math.max(nums[0], nums[1]);
+    for(let i=2; i<nums.length; i++){
+        const maxAtCurrent = Math.max(nums[i]+maxAtTwoBefore, maxAtOneBefore);
+        maxAtTwoBefore = maxAtOneBefore;
+        maxAtOneBefore = maxAtCurrent;
+    }
+    return maxAtOneBefore;
+};
