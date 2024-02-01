5c8fa59c914bb02ea28934d1f2ff26e6f8f83cd8
Abhinav Thapa <abhinavthapa1998@gmail.com>
2024-02-01 19:12:35 +0530

    Time: 356 ms (20.00%) | Memory: 82.6 MB (100.00%) - LeetSync

---
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.ts
@@ -0,0 +1,11 @@
+function divideArray(nums: number[], k: number): number[][] {
+    const sortedNums = [...nums].sort((a,b)=>a-b);
+    const ans = [];
+    for(let i=0; i<sortedNums.length; i+=3){
+        if (sortedNums[i+2] - sortedNums[i] > k){
+            return [];
+        }
+        ans.push([sortedNums[i], sortedNums[i+1], sortedNums[i+2]]);
+    }
+    return ans;
+};
