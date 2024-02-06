81bddc39086f7bb2a1cad1bce38e95e8e11851e5
Abhinav Thapa <abhinavthapa1998@gmail.com>
2024-02-06 10:01:53 +0530

    Time: 136 ms (23.32%) | Memory: 64.9 MB (6.70%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.ts
@@ -0,0 +1,11 @@
+
+function groupAnagrams(strs: string[]): string[][] {
+  let map = {};
+  for(let str of strs){
+      let s = str.split('').sort().join('');
+      if(!map[s])map[s] = []
+      map[s].push(str)
+  }
+  return Object.values(map)
+};
+
