337e1f346c7b8d0c19015e8c112814f4f4060534
scarletczen <abhinavthapa1998@gmail.com>
2024-02-14 19:26:30 +0530

    Time: 331 ms (28.38%) | Memory: 98.2 MB (31.08%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.ts
@@ -0,0 +1,10 @@
+// Chad JS Solution
+function rearrangeArray(nums: number[]): number[] {
+    let positiveNums = nums.filter(x=>x>0);
+    let negativeNums = nums.filter(x=>x<0);
+    let result = [];
+    for(let i=0;i<nums.length/2;i++){
+        result.push(positiveNums[i], negativeNums[i])
+    }
+    return result;
+};
